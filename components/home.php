e1dc37761b651087e84b2ff37ac9feadd6bcca6c
DESKTOP-GTEC5V2\devzain <geekspod@gmail.com>
2018-07-02 17:41:13 +0500

    no message

---
+++ components/home.php
@@ -0,0 +1,63 @@
+<section class="mast-head universal-section">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-4">
+
+            </div>
+            <div class="col-lg-4">
+
+            </div>
+            <div class="col-lg-4">
+
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="pjc-service universal-section">
+    <div class="container">
+        <div class="row">
+
+        </div>
+    </div>
+</section>
+
+<section class="use-cases universal-section">
+    <div class="container">
+        <div class="row">
+
+        </div>
+    </div>
+</section>
+
+<section class="jet-card universal-section">
+    <div class="container">
+        <div class="row">
+
+        </div>
+    </div>
+</section>
+
+<section class="virtual-reality universal-section">
+    <div class="container">
+        <div class="row">
+
+        </div>
+    </div>
+</section>
+
+<section class="token-summary universal-section">
+    <div class="container">
+        <div class="row">
+
+        </div>
+    </div>
+</section>
+
+<section class="road-map universal-section">
+    <div class="container">
+        <div class="row">
+
+        </div>
+    </div>
+</section>
