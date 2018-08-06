6db1869b7fb1beadf929de0093ba22521a965fa8
Qaiser <>
2018-08-06 14:45:22 +0500

    zip folder added

--- src-assets/javascripts/components/home.js
+++ src-assets/javascripts/components/home.js
@@ -11,8 +11,8 @@ function prevInfo(divhide,divshow) {
 
 //------Uploading zip and jet animation section Below------
 
-// var loader = new ZipLoader('filess.zip');
-
+ var loader = new ZipLoader('Poses.zip');
+debugger;
 // // THIS SETS UP THE INITIAL ARRAY VARIABLE
 // // Array of images to swap between
 // var images = [];
@@ -20,22 +20,22 @@ function prevInfo(divhide,divshow) {
 // // THIS LOOKS AT THE DATA ATTRIBUTES IN THE load element AND GENERATES 
 // // THE IMAGE NAMES BASED ON A GIVEN RANGE 
 // // THEN PUSHES THEM INTO THE ARRAY
-// for (i = 1; i <= $('#load').attr('data-images'); i++) {
-//     debugger;
-//     images.push(loader.loadImage('filess.zip://images/' + i + '.png'));
-// }
+//  for (i = 1; i <= $('#load').attr('data-images'); i++) {
+//      debugger;
+//      images.push(loader.loadImage('filess.zip://images/' + i + '.png'));
+//  }
 
 
 // // THIS TAKES EACH IMAGE NAME FROM THE ARRAY AND CREATES A CLUSTER OF
 // // PRELOADED HIDDEN IMAGES ON THE PAGE USING JQUERY
-// $(images).each(function () {
-//     $('<img />')[0].src = this;
-// });
+//  $(images).each(function () {
+//      $('<img />')[0].src = this;
+//  });
 
 
-// var totalImages = images.length;
+//  var totalImages = images.length;
 
-// var pageHeight = $(document).height() - $(window).height();
+//  var pageHeight = $(document).height() - $(window).height();
 
 // // Work out how often we should change image (i.e. how far we scroll between changes)
 // var scrollInterval = Math.floor(pageHeight / totalImages);
